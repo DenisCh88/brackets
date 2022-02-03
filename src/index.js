eeddea3c5c55059b3efc28fdff56e887e65f1f94
Денис Чухліб <dchuhleb@icloud.com>
2022-02-03 23:12:31 +0200

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,75 @@
 module.exports = function check(str, bracketsConfig) {
-
-  let brackets = bracketsConfig.join('').replace(/,/g, '');
-  let stack = [];
-  for (let bracket of str) {
-    let bracketsIndex = brackets.indexOf(bracket)
-
-    if (bracketsIndex % 2 === 0) {
-
-      if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex) {
-        stack.pop();
-      } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
-        stack.push(bracketsIndex)
-      }
-      else {
-        stack.push(bracketsIndex)
-      }
+  let strArr = str.split('');
+   
+   
+ for (let i = 0; i < bracketsConfig.length; i++){ 
+   let bracketsConfigItem = bracketsConfig[i];
+   for ( let k = 0; k < bracketsConfigItem.length; k++ ){
+     for ( let l = 0; l < strArr.length; l++){
+       if (strArr[l] === bracketsConfigItem[k] && strArr[l + 1] === bracketsConfigItem[k + 1] && strArr[l + 1] !== undefined){
+         strArr.splice(l, 2);
+         l = 0;
+         k = 0;
+         i = 0;
+       } 
+     }
+   }
+ }
+   for (let i = 0; i < bracketsConfig.length; i++){ 
+   let bracketsConfigItem = bracketsConfig[i];
+   for ( let k = 0; k < bracketsConfigItem.length; k++ ){
+     for ( let l = 0; l < strArr.length; l++){
+       if (strArr[l] === bracketsConfigItem[k] && strArr[l + 1] === bracketsConfigItem[k + 1] && strArr[l + 1] !== undefined){
+         strArr.splice(l, 2);
+         l = 0;
+         k = 0;
+         i = 0;
+       } 
+     }
+   }
+ }
+ for (let i = 0; i < bracketsConfig.length; i++){ 
+  let bracketsConfigItem = bracketsConfig[i];
+  for ( let k = 0; k < bracketsConfigItem.length; k++ ){
+    for ( let l = 0; l < strArr.length; l++){
+      if (strArr[l] === bracketsConfigItem[k] && strArr[l + 1] === bracketsConfigItem[k + 1] && strArr[l + 1] !== undefined){
+        strArr.splice(l, 2);
+        l = 0;
+        k = 0;
+        i = 0;
+      } 
     }
-    else {
-      if (stack.pop() !== bracketsIndex - 1) {
-        return false;
-      }
-    }
-
   }
-  return stack.length === 0;
-
+}
+for (let i = 0; i < bracketsConfig.length; i++){ 
+  let bracketsConfigItem = bracketsConfig[i];
+  for ( let k = 0; k < bracketsConfigItem.length; k++ ){
+    for ( let l = 0; l < strArr.length; l++){
+      if (strArr[l] === bracketsConfigItem[k] && strArr[l + 1] === bracketsConfigItem[k + 1] && strArr[l + 1] !== undefined){
+        strArr.splice(l, 2);
+        l = 0;
+        k = 0;
+        i = 0;
+      } 
+    }
+  }
+}
+for (let i = 0; i < bracketsConfig.length; i++){ 
+  let bracketsConfigItem = bracketsConfig[i];
+  for ( let k = 0; k < bracketsConfigItem.length; k++ ){
+    for ( let l = 0; l < strArr.length; l++){
+      if (strArr[l] === bracketsConfigItem[k] && strArr[l + 1] === bracketsConfigItem[k + 1] && strArr[l + 1] !== undefined){
+        strArr.splice(l, 2);
+        l = 0;
+        k = 0;
+        i = 0;
+      } 
+    }
+  }
+}
+  if (strArr.length > 0){
+    return false
+  } else {
+    return true
+  }
 }
